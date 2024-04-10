391c66fb31ba5cf0b75096ba57ab710811b0c969
mbsurfer <rneldner@gmail.com>
2024-04-10 19:54:29 -0400

    added google cloud deployment

--- .github/actions/deploy-cloud-storage-javascript/main.js
+++ .github/actions/deploy-cloud-storage-javascript/main.js
@@ -4,6 +4,20 @@ const exec = require('@actions/exec');
 
 function run () {
   core.notice('Deploying to Google Cloud Storage...');
+
+  const bucket = core.getInput('bucket', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+  const gcpKey = core.getInput('gcp-key', { required: true });
+
+  // Write the key to a file
+  const keyPath = './gcp-key.json';
+  fs.writeFileSync(keyPath, gcpKey);
+
+  // Authenticate with gcloud
+  exec.exec(`gcloud auth activate-service-account --key-file=${keyPath}`);
+
+  const cloudStorageUri = `gs://${bucket}`;
+  exec.exec(`gcloud storage cp ${distFolder} ${cloudStorageUri}`);
 }
 
 run();
